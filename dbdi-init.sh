d1395941de74806ffb16128e17b87dae2b18f8a4
Tim Bunce <Tim.Bunce@pobox.com>
2010-09-06 21:47:11 +0100

    moved modules into new lib dir

--- dbdi-init.sh
+++ dbdi-init.sh
@@ -1,5 +1,5 @@
 # source into your shell
-export PERL6LIB=".:jdbclib:$PERL6LIB"
+export PERL6LIB=".:lib:jdbclib:$PERL6LIB"
 
 # Also need to ensure the native postgres libpq is loadable
 # For OS X you'll need something like this in your ~/.profile
